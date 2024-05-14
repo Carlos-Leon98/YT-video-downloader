cf1d7ef1b58f79525e621b00beb718629d838e8d
cleonrivas <carlosaleonrivas@gmail.com>
2024-05-13 23:55:42 -0500

    Enhancing error handling in downloader.py

--- src/controllers/downloader.py
+++ src/controllers/downloader.py
@@ -13,11 +13,11 @@ def downloadVideo(savePath, link):
     """
     try:
         yt = YouTube(link) # Create a YouTube object with the provided URL
-    except:
-        print("Connection Error") # Print error message if connection fails
+    except Exception as e:
+        print("Error creating Youtube object:", e) # Print error message if connection fails
 
     #  Get all streams and filter for mp4 files
-    mp4_streams = yt.streams.filter(file_extension='mp4').all()
+    mp4_streams = yt.streams.filter(file_extension='mp4')
 
     # Get the video with the highest resolution
     d_video = mp4_streams[-1]
@@ -26,5 +26,5 @@ def downloadVideo(savePath, link):
         # Download the video to the specified savePath
         d_video.download(output_path=savePath)
         print('Video downloaded successfully!') # Print success message
-    except:
-        print("Some Error!") # Print error message if download fails
+    except Exception as e:
+        print("Error downloading video:", e) # Print error message if download fails
