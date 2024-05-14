631727028f8b77f0877878dc82225b4ff1a2b675
cleonrivas <carlosaleonrivas@gmail.com>
2024-05-13 23:41:12 -0500

    Adding comments to main.py within src/ directory

--- src/main.py
+++ src/main.py
@@ -1,7 +1,25 @@
-import interface as i
+import interface as i # Import the interface module and alias it as i
 
 def run():
-    i.set_user_interface()
+    """
+    Function to run the YouTube downloader application.
+    
+    This function calls the set_user_interface function from the interface module
+    to start the user interface of the application.
+    
+    Args:
+        None
+        
+    Returns:
+        None
+    """
+    i.set_user_interface() # Call the set_user_interface function
 
 if __name__ == '__main__':
-    run()
+    """
+    Entry point of the application.
+    
+    If this script is executed directly (not imported as a module),
+    the run function is called to start the application.
+    """
+    run() # Call the run function to start the application
