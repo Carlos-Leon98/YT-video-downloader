6c01c850de5004493bf55e7359ffbe7eaa2c692b
cleonrivas <carlosaleonrivas@gmail.com>
2024-05-13 19:54:41 -0500

    Adding comments to document downloader.py

--- src/controllers/downloader.py
+++ src/controllers/downloader.py
@@ -1,10 +1,20 @@
-from pytube import Youtube
+from pytube import Youtube  # Import the YouTube class from the pytube library
 
 def downloadVideo(savePath, link):
+    """
+    Download a YouTube video given its URL.
+
+    Args:
+        savePath (str): The directory where the video will be saved.
+        link (str): The URL of the YouTube video.
+
+    Returns:
+        None
+    """
     try:
-        yt = Youtube(link)
+        yt = Youtube(link) # Create a YouTube object with the provided URL
     except:
-        print("Connection Error")
+        print("Connection Error") # Print error message if connection fails
 
     #  Get all streams and filter for mp4 files
     mp4_streams = yt.streams.filter(file_extension='mp4').all()
@@ -13,7 +23,8 @@ def downloadVideo(savePath, link):
     d_video = mp4_streams[-1]
 
     try:
+        # Download the video to the specified savePath
         d_video.download(output_path=savePath)
-        print('Video downloaded successfully!')
+        print('Video downloaded successfully!') # Print success message
     except:
-        print("Some Error!")
+        print("Some Error!") # Print error message if download fails
